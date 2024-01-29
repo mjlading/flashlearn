4daf7c47b428380e30bdf15be283822ccbe49e6c
Mats Lading <mjlading@ThinkPadMats>
2024-01-29 19:12:48 +0100

    feat: set up Prisma with Prisma adapter for Auth.js

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,11 @@
 import NextAuth, { NextAuthConfig } from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "./db";
 
 export const authConfig = {
   providers: [GitHub],
+  adapter: PrismaAdapter(prisma),
 } satisfies NextAuthConfig;
 
 export const {

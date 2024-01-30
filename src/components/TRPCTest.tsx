ba516d35b63c3a4df6c3ad984645ee0d442792c2
Mats Lading <mjlading@ThinkPadMats>
2024-01-30 09:02:49 +0100

    test: temporarily display tRPC and Prisma data on main page for testing

--- src/components/TRPCTest.tsx
+++ src/components/TRPCTest.tsx
@@ -4,6 +4,15 @@ import { trpc } from "@/app/api/trpc/client";
 
 export default function TRPCTest() {
   const helloTest = trpc.hello.useQuery();
+  const prismaTest = trpc.helloPrisma.useQuery();
 
-  return <>{helloTest.data}</>;
+  return (
+    <>
+      {helloTest.data}
+      <br />
+      <br />
+      Prisma test (fetch first db user):
+      <pre>{JSON.stringify(prismaTest.data, null, 2)}</pre>
+    </>
+  );
 }
--- src/server/routers/index.ts
+++ src/server/routers/index.ts
@@ -4,6 +4,10 @@ export const appRouter = router({
   hello: publicProcedure.query(() => {
     return "Hello from tRPC!";
   }),
+  helloPrisma: publicProcedure.query(async () => {
+    // Test prisma db
+    return await prisma?.user.findFirst();
+  }),
 });
 
 export type AppRouter = typeof appRouter;

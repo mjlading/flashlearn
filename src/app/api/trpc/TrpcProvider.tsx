49204fd41dca25adb1100ef08c77c826b6145c17
Mats Lading <mjlading@ThinkPadMats>
2024-01-30 10:21:24 +0100

    fix: resolve issue with dynamic tRPC client URL, set correct URL

--- src/app/api/trpc/TrpcProvider.tsx
+++ src/app/api/trpc/TrpcProvider.tsx
@@ -7,7 +7,7 @@ import { trpc } from "./client";
 
 const TRPC_API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://hello-there-git-main-matslading.vercel.app/api/trpc"
+    ? "https://learning-app-one-mu.vercel.app/api/trpc"
     : "http://localhost:3000/api/trpc";
 
 export default function TrpcProvider({ children }: PropsWithChildren) {

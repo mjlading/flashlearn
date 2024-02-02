ab0f2f1a40f5d96fd11dff1f130b20b41c77cd31
Mats Lading <mats.lading@gmail.com>
2024-02-02 15:20:41 +0100

    refac(test): remove trpc test

--- src/__tests__/method.test.ts
+++ src/__tests__/method.test.ts
@@ -8,20 +8,6 @@ test("adds 1 + 2 to equal 3", () => {
   expect(1 + 2).toBe(3);
 });
 
-test("test trpc", async () => {
-  const expected = {
-    email: "mjlading@stud.ntnu.no",
-    emailVerified: null,
-    id: "cls03t74k00001511wa6ffq0k",
-    image: "https://avatars.githubusercontent.com/u/91284776?v=4",
-    name: "mjlading",
-  };
-
-  const actual = await testProxy.test.helloPrisma.query();
-
-  expect(actual).toStrictEqual(expected);
-});
-
 test("test protected procedure", async () => {
   // const actual = await testProxy.test.protectedTest.query();
 

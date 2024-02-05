25a8e715466deda78d85629fbf27e7266d1110c2
Mats Lading <mats.lading@gmail.com>
2024-02-05 19:02:56 +0100

    feat(api): add procedure for creating a deck

--- src/server/routers/deck.ts
+++ src/server/routers/deck.ts
@@ -1,4 +1,25 @@
-import { publicProcedure, router } from "../trpc";
+import { protectedProcedure, publicProcedure, router } from "../trpc";
 import z from "zod";
+import type { Prisma } from "@prisma/client";
 
-export const deckRouter = router({});
+const createDeck = z.object({
+  name: z.string(),
+  isPublic: z.boolean().default(true),
+  averageRating: z.number(),
+  academicLevel: z.number(),
+  subjectName: z.string(),
+});
+
+export const deckRouter = router({
+  createDeck: protectedProcedure
+    .input(createDeck)
+    .mutation(async ({ ctx, input }) => {
+      const newDeck = await ctx.prisma.deck.create({
+        data: {
+          ...input,
+          userId: ctx.session.user.id,
+        },
+      });
+      return newDeck;
+    }),
+});

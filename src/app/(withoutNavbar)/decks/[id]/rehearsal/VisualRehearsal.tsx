9d3460daaff11c19ac6d90cbfe04c72b2dd4a8d6
Mats Lading <mats.lading@gmail.com>
2024-02-16 13:03:07 +0100

    feat: move flashcard code into reusable component, add card flip style

--- src/app/(withoutNavbar)/decks/[id]/rehearsal/VisualRehearsal.tsx
+++ src/app/(withoutNavbar)/decks/[id]/rehearsal/VisualRehearsal.tsx
@@ -1,20 +1,17 @@
 "use client";
 
-import { api } from "@/app/api/trpc/client";
+import Flashcard from "@/components/Flashcard";
 import { Progress } from "@/components/ui/progress";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { Flashcard } from "@prisma/client";
-import { useParams, useSearchParams } from "next/navigation";
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useState } from "react";
+import { type Flashcard as FlashcardType } from "@prisma/client";
 
 export default function VisualRehearsal({
   flashcards,
 }: {
-  flashcards: Flashcard[];
+  flashcards: FlashcardType[];
 }) {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [currentFlashcard, setCurrentFlashcard] = useState(flashcards[0]);
-  const [showBack, setShowBack] = useState(false);
   const [progress, setProgress] = useState(0);
 
   useEffect(() => {
@@ -28,16 +25,12 @@ export default function VisualRehearsal({
   });
 
   function handleKeydown(event: KeyboardEvent) {
-    if (event.code === "Space") {
-      // Flip card
-      setShowBack(!showBack);
-    } else if (event.code === "ArrowRight") {
+    if (event.code === "ArrowRight") {
       // Go to next card
       const nextIndex = currentIndex + 1;
 
       if (nextIndex >= flashcards.length) return;
 
-      setShowBack(false); // Always show front on next card
       setCurrentIndex(nextIndex);
     } else if (event.code === "ArrowLeft") {
       // Go to previous card
@@ -45,50 +38,14 @@ export default function VisualRehearsal({
 
       if (nextIndex < 0) return;
 
-      setShowBack(false); // Always show front on previous card
       setCurrentIndex(nextIndex);
     }
   }
 
-  function dynamicTextSize(textLength: number): string {
-    if (textLength < 50) {
-      return "text-xl";
-    } else if (textLength < 100) {
-      return "text-lg";
-    } else if (textLength < 500) {
-      return "text-md";
-    } else {
-      return "text-sm";
-    }
-  }
-
-  // useMemo to prevent recalculating these on every render
-  const textSizeFront = useMemo(
-    () => dynamicTextSize(currentFlashcard.front.length),
-    [currentFlashcard.front]
-  );
-  const textSizeBack = useMemo(
-    () => dynamicTextSize(currentFlashcard.back.length),
-    [currentFlashcard.back]
-  );
-
   return (
     <>
       <Progress value={progress} />
-      <div
-        onClick={() => setShowBack(!showBack)}
-        className="h-[20rem] w-[30rem] rounded-xl bg-muted/40 p-7 leading-relaxed break-words mt-8"
-      >
-        {/* The front of the flashcard */}
-        <ScrollArea hidden={showBack} className="h-full">
-          <p className={textSizeFront}>{currentFlashcard.front}</p>
-        </ScrollArea>
-
-        {/* The back of the flashcard */}
-        <ScrollArea hidden={!showBack} className="h-full">
-          <p className={textSizeBack}>{currentFlashcard.back}</p>
-        </ScrollArea>
-      </div>
+      <Flashcard flashcard={currentFlashcard} />
     </>
   );
 }
---
+++ src/components/Flashcard.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { type Flashcard } from "@prisma/client";
+import { useEffect, useMemo, useState } from "react";
+import { ScrollArea } from "./ui/scroll-area";
+
+export default function Flashcard({ flashcard }: { flashcard: Flashcard }) {
+  const [showBack, setShowBack] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeydown);
+    return () => window.removeEventListener("keydown", handleKeydown);
+  });
+
+  function handleKeydown(event: KeyboardEvent) {
+    if (event.code === "Space") {
+      // Flip card
+      setShowBack(!showBack);
+    }
+  }
+
+  function dynamicTextSize(textLength: number): string {
+    if (textLength < 50) {
+      return "text-xl";
+    } else if (textLength < 100) {
+      return "text-lg";
+    } else if (textLength < 500) {
+      return "text-md";
+    } else {
+      return "text-sm";
+    }
+  }
+
+  // useMemo to prevent recalculating these on every render
+  const textSizeFront = useMemo(
+    () => dynamicTextSize(flashcard.front.length),
+    [flashcard.front]
+  );
+  const textSizeBack = useMemo(
+    () => dynamicTextSize(flashcard.back.length),
+    [flashcard.back]
+  );
+
+  // Flip animation
+  const flipStyle: React.CSSProperties = {
+    transform: showBack ? "rotateX(180deg)" : "rotateX(0deg)",
+    transition: "transform 0.5s cubic-bezier(0.175, 0.885, 0.32, 1)",
+  };
+
+  const frontStyle: React.CSSProperties = {
+    opacity: showBack ? 0 : 1,
+    zIndex: showBack ? 1 : 2,
+    transition: "opacity 0.5s",
+  };
+
+  const backStyle: React.CSSProperties = {
+    opacity: showBack ? 1 : 0,
+    zIndex: showBack ? 2 : 1,
+    transition: "opacity 0.5s",
+    transform: "rotateX(180deg)",
+    position: "absolute",
+    top: 0,
+    left: 0,
+  };
+
+  return (
+    <div
+      onClick={() => setShowBack(!showBack)}
+      className="h-[20rem] w-[30rem] rounded-xl bg-muted/40 leading-relaxed break-words mt-8"
+      style={flipStyle}
+    >
+      {/* The front of the flashcard */}
+      <ScrollArea style={frontStyle} className="h-full w-full p-7">
+        <p className={textSizeFront}>{flashcard.front}</p>
+      </ScrollArea>
+
+      {/* The back of the flashcard */}
+      <ScrollArea style={backStyle} className="h-full w-full p-7">
+        <p className={textSizeBack}>{flashcard.back}</p>
+      </ScrollArea>
+    </div>
+  );
+}

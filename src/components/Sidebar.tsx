cd84ecccd52fda7b85066f960eddd489cc587c2b
Mats Lading <mats.lading@gmail.com>
2024-02-22 11:58:29 +0100

    refac(ui): minor sidebar improvement

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -39,18 +39,19 @@ export default function Sidebar() {
 
   return (
     <aside className="sticky top-[61px] height-minus-navbar overflow-none border-r min-w-[10rem] lg:min-w-[15rem]">
-      <nav className="pt-7 px-3 lg:px-7 flex flex-col gap-2">
+      <nav className="pt-5 px-3 lg:px-5 flex flex-col gap-2">
         {links.map((link) => (
           <Link
             key={link.href}
             href={link.href + (link.query ? link.query : "")}
             className={cn(
-              pathname == link.href ? "bg-accent text-accent-foreground" : "",
-              "rounded-md px-3 py-2 flex gap-2 items-center hover:bg-accent hover:text-accent-foreground"
+              pathname == link.href ? "bg-accent" : "opacity-80",
+              "rounded-lg px-3 py-1.5 flex font-medium gap-4 items-center text-lg hover:bg-accent transition"
             )}
+            style={pathname == link.href ? { fontWeight: 600 } : {}}
           >
             {link.icon}
-            <span className="font-medium text-sm">{link.text}</span>
+            <span className="text-[0.93rem]">{link.text}</span>
           </Link>
         ))}
       </nav>

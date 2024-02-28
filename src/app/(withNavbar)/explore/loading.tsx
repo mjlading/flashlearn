81e00e071df29233680c1ed8f95d1e04caa9f660
Mats Lading <mats.lading@gmail.com>
2024-02-28 13:11:54 +0100

    feat: improve subject page loading.tsx

--- src/app/(withNavbar)/explore/loading.tsx
+++ src/app/(withNavbar)/explore/loading.tsx
@@ -3,7 +3,8 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function Loading() {
   return (
     <div className="w-[50rem]">
-      <Skeleton className="h-[4rem] w-[25rem] mb-12" />
+      <Skeleton className="h-[3rem] w-[14rem] mb-4" />
+      <Skeleton className="h-[1rem] w-[22rem] mb-12" />
       <div className="grid grid-cols-3 gap-8">
         {Array.from({ length: 9 }).map((_, i) => (
           <Skeleton key={i} className="h-[7rem]" />

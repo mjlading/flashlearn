f070f5f99a0c9b833f1882ded6c8bc49317b44d9
Mats Lading <mats.lading@gmail.com>
2024-03-22 16:44:24 +0100

    feat: no collections case covered

--- src/components/CollectionList.tsx
+++ src/components/CollectionList.tsx
@@ -3,6 +3,9 @@
 import { api } from "@/app/api/trpc/client";
 import CollectionCard from "./CollectionCard";
 import { Skeleton } from "./ui/skeleton";
+import Link from "next/link";
+import { Plus, SquareStack } from "lucide-react";
+import { buttonVariants } from "./ui/button";
 
 function CollectionListSkeleton() {
   return (
@@ -14,11 +17,40 @@ function CollectionListSkeleton() {
   );
 }
 
+function NoCollections() {
+  return (
+    <div className="h-full text-center flex flex-col items-center justify-center gap-4">
+      <h2 className="font-semibold text-lg">Du har ingen samlinger</h2>
+      <p className="text-muted-foreground">
+        Lag en samling for å organisere og øve flere sett sammen
+      </p>
+      <Link
+        href="/dashboard"
+        className={buttonVariants({
+          size: "lg",
+          className: "mt-8",
+        })}
+      >
+        <Plus size={20} className="mr-1" />
+        Ny samling
+      </Link>
+
+      <div className="mt-8">
+        <SquareStack size={80} opacity={0.2} />
+      </div>
+    </div>
+  );
+}
+
 export default function CollectionList() {
   const collections = api.collection.getUserCollections.useQuery();
 
   if (collections.isLoading) {
     return <CollectionListSkeleton />;
+  }
+
+  if (collections.data.length === 0) {
+    return <NoCollections />;
   }
 
   return (

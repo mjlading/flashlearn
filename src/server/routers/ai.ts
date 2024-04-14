19b80dff5cfd4372cac0a1dc7afad375069709c4
Mats Lading <mats.lading@gmail.com>
2024-04-14 14:22:21 +0200

    feat(api): upgrade from gpt-3.5 turbo to gpt-4-turbo-preview

--- src/server/routers/ai.ts
+++ src/server/routers/ai.ts
@@ -186,7 +186,7 @@ export const aiRouter = router({
       ];
 
       const completion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
+        model: "gpt-4-turbo-preview",
         tools: tools,
         tool_choice: { type: "function", function: { name: "generate_tags" } }, // Forces function_call
         messages: [
@@ -274,7 +274,7 @@ export const aiRouter = router({
       ];
 
       const completion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
+        model: "gpt-4-turbo-preview",
         tools: tools,
         tool_choice: {
           type: "function",
@@ -361,7 +361,7 @@ export const aiRouter = router({
       ];
 
       const completion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
+        model: "gpt-4-turbo-preview",
         tools: tools,
         tool_choice: {
           type: "function",
@@ -429,7 +429,7 @@ export const aiRouter = router({
       ];
 
       const completion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
+        model: "gpt-4-turbo-preview",
         tools: tools,
         tool_choice: {
           type: "function",

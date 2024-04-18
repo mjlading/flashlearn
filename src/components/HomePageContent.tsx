bd489b447c8548becc3dbc46cc1dc034c32644c1
Mats Lading <mats.lading@gmail.com>
2024-04-18 13:12:46 +0200

    feat: minor UI changes

--- src/components/HomePageContent.tsx
+++ src/components/HomePageContent.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { motion } from "framer-motion";
 import AuthButton from "./AuthButton";
+import { Button, buttonVariants } from "./ui/button";
+import Link from "next/link";
 
 export default function HomePageContent({ dict }: { dict: any }) {
   return (
@@ -14,11 +16,11 @@ export default function HomePageContent({ dict }: { dict: any }) {
       }}
       className="relative flex flex-col gap-4 items-center justify-center px-4"
     >
-      <h1 className="text-5xl font-bold text-gray-900 mb-6">
+      <h1 className="text-5xl font-bold text-gray-900 dark:text-slate-100 mb-6">
         {dict.home.title}
       </h1>
       <h3 className="text-xl text-gray-600 mb-12">{dict.home.slogan}</h3>
-      <div className="p-4 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+      <div className="p-5 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
         <AuthButton dict={dict} />
       </div>
     </motion.div>

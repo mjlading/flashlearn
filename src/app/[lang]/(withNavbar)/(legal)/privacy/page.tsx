bbc6f45115b790a01404e261cf5ed0ed7593c4d1
Gunnar Antoni Solli Olsen <gaolsen@stud.ntnu.no>
2024-05-09 18:22:10 +0200

    feat: added content to privacy and ToS page

--- src/app/[lang]/(withNavbar)/(legal)/privacy/page.tsx
+++ src/app/[lang]/(withNavbar)/(legal)/privacy/page.tsx
@@ -1,7 +1,13 @@
 export default function PrivacyPage() {
   return (
     <>
-      <h1>Personvern Side</h1>
+      <h1>Flashlearn Privacy policy, Effective 09.05.2024</h1>
+
+      <h1>This document contains Flashlearn's policy on how we store and use your personal information. It applies to all services provided through Flashlearn.no</h1>
+
+      <h1>When you sign in with a third party account (We currently support Github and Google) for the purposes of authentication we store your name, email and profile picture associated with your chosen account provider, along with a nickname of your choosing. </h1>
+
+      <h1>We use SupaBase as a database provider, this means that your information is stored in a postgres database.</h1>
     </>
   );
 }
--- src/app/[lang]/(withNavbar)/(legal)/terms/page.tsx
+++ src/app/[lang]/(withNavbar)/(legal)/terms/page.tsx
@@ -1,7 +1,15 @@
 export default function TermsPage() {
   return (
     <>
-      <h1>BrukervilkårSide</h1>
+      <h1>Flashlearn terms of service, Effective 09.05.2024</h1>
+
+      <h1>This document contains Flashlearn's policy on how you can use the services provided on the site 'Flashlearn.no'</h1>
+
+      <h1>We reserve the right to remove any users that abuse our services, example given:</h1>
+
+      <h1>- Use of the card generation feature for purposes other than using the generated flashcards</h1>
+
+      <h1>- Explicit language on manually created cards</h1>
     </>
   );
 }

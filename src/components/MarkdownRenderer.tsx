e815739c2181716bfdc6d1ecc0c39ca8a71990fb
Mats Lading <mats.lading@gmail.com>
2024-05-18 19:02:27 +0200

    feat: add dark mode syntax highlighter

--- src/components/MarkdownRenderer.tsx
+++ src/components/MarkdownRenderer.tsx
@@ -1,11 +1,13 @@
+"use client";
+
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 import "katex/dist/katex.min.css";
 import rehypeHighlight from "rehype-highlight";
-import "highlight.js/styles/github.css";
 import rehypeRaw from "rehype-raw";
+import { useTheme } from "next-themes";
 
 /**
  * Markdown renderer. Can render markdown and LaTex and has syntax highlighting for code.
@@ -15,6 +17,15 @@ export default function MarkdownRenderer({
 }: {
   children: string | undefined;
 }) {
+  const { theme } = useTheme();
+
+  // Dynamically import the correct Highlight.js theme css
+  if (theme === "dark") {
+    import("highlight.js/styles/github-dark.css");
+  } else {
+    import("highlight.js/styles/github.css");
+  }
+
   return (
     <Markdown
       remarkPlugins={[remarkGfm, remarkMath]}
